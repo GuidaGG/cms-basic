6dc7bb3e980a66db08c0b0045e3f829d204c8cfa
guidagg <guida.gribeiro@gmail.com>
2025-07-10 13:45:00 +0200

    feat: add nodemailer simple setup

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_USERNAME'),
+        defaultReplyTo: env('SMTP_USERNAME'),
+      },
+    },
+  },
+});
